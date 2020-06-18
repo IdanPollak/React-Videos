f534f140ee32de2afc78552677d02db46d9f4fbd
Idan Pollak <idanpol1@gmail.com>
2020-06-18 09:44:03 +0300

    Updates

---
+++ precache-manifest.a11efd64d363b3cd7e6a5693698afb5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff0edcc29f64b10e7e7d28df490ed38a",
+    "url": "/React-WeatherApp/index.html"
+  },
+  {
+    "revision": "8d25f615237a5d17c88a",
+    "url": "/React-WeatherApp/static/css/main.1b76f6e5.chunk.css"
+  },
+  {
+    "revision": "86e90bff000bb62f17ec",
+    "url": "/React-WeatherApp/static/js/2.fdc1b8b7.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/React-WeatherApp/static/js/2.fdc1b8b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d25f615237a5d17c88a",
+    "url": "/React-WeatherApp/static/js/main.41aad63b.chunk.js"
+  },
+  {
+    "revision": "646c4a195c9d72d14de5",
+    "url": "/React-WeatherApp/static/js/runtime-main.18d297ed.js"
+  }
+]);
